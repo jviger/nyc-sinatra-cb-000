6e72f6f06186a717fd147dcef5841f943d439c8b
Jason Viger <jason.viger@gmail.com>
2018-09-04 16:20:40 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,7 +16,7 @@ class FiguresController < ApplicationController
   end
   
   get "/figures/:id" do 
-    @figure = Figure.find_by(params[:id])
+    @figure = Figure.find(params[:id])
     erb :"/figures/show"
   end
   
